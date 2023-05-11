386a3ad051ff52cf4107e9494ff372db02cf60ad
leemhyungyu <lhglbkksj@naver.com>
2023-05-11 17:05:01 +0900

    [style] 폴더 정리

--- capston-inu-iOS/capston-inu-iOS/Source/Presentation/Login/ViewController/LoginViewController.swift
+++ capston-inu-iOS/capston-inu-iOS/Source/Presentation/Login/ViewController/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  capston-inu-iOS
 //
 //  Created by 임현규 on 2023/05/11.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
--- capston-inu-iOS/capston-inu-iOS/Source/SceneDelegate.swift
+++ capston-inu-iOS/capston-inu-iOS/Source/SceneDelegate.swift
@@ -20,8 +20,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         window = UIWindow(windowScene: windowScene)
 
-        let vc = ViewController()
-        window?.rootViewController = vc
+        let loginViewController = LoginViewController()
+        window?.rootViewController = loginViewController
         window?.makeKeyAndVisible()
     }
 
